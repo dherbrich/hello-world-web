25c3d9bb8fa4cc1b6ba66a76a914b61d550d85c6
dherbrich <dherbrich@pivotal.io>
2019-05-28 09:22:01 +0200

    clean up

--- cmd/hello-world-web/hello-world-web.go
+++ cmd/hello-world-web/hello-world-web.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"fmt"
+	"github.com/dherbrich/hello-world-pkg"
+	"github.com/dherbrich/hello-world-web/internal/hellocontroller"
+	"github.com/gorilla/mux"
+	"log"
 	"net/http"
-
-	"github.com/dherbric/hello-world-web/internal/hellocontroller"
+	"os"
 )
 
 func main() {
-	http.HandleFunc("/", hellocontroller.Greet)
-	http.HandleFunc("/healthz", hellocontroller.Healthz)
-	http.HandleFunc("/healthz/switch", hellocontroller.SwitchHealthzWeb)
+	port := os.Getenv("HELLO_PORT")
 
-	hellocontroller.SwitchHealthz()
-	http.ListenAndServe(":8080", nil)
+	router := mux.NewRouter()
+
+	subrouterHello1 := router.PathPrefix("/hello1").Subrouter()
+	subrouterHello1.Methods("GET").HandlerFunc(HandleGetHello1)
+
+	subrouterHello2 := router.HandleFunc("/hello2", HandleGetHello2).Subrouter()
+	subrouterHello2.Methods("GET")
+
+	subrouterHello3 := router.HandleFunc("/hello3", hellocontroller.Greet).Subrouter()
+	subrouterHello3.Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
+
+func HandleGetHello1(w http.ResponseWriter, _ *http.Request) {
+	response := hello.SayHello("from 1")
+	fmt.Fprintf(w, response)
+}
+
+func HandleGetHello2(w http.ResponseWriter, _ *http.Request) {
+	response := hello.SayHello("from 2")
+	fmt.Fprintf(w, response)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
-module github.com/dherbric/hello-world-web
+module github.com/dherbrich/hello-world-web
 
-require github.com/dherbrich/hello-world-pkg v0.0.0-20190413211021-409c828b00ac
+require (
+	github.com/dherbrich/hello-world-pkg v0.0.0-20190413211021-409c828b00ac
+	github.com/gorilla/mux v1.7.1
+)
+
+go 1.12
+
+//replace github.com/dherbrich/hello-world-pkg => ../hello-world-pkg/
